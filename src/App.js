06c8965d0e0d5b7956a81c8cd6daca2e1b0629a4
Aleff-Espinosa-Cordova <espinosacordovaaleff@gmail.com>
2021-12-06 18:16:13 -0600

    master

--- src/App.js
+++ src/App.js
@@ -14,12 +14,10 @@ import {
 } from "reactstrap";
 
 const data = [
-  { id: 1, personaje: "Naruto", anime: "Naruto" },
-  { id: 2, personaje: "Goku", anime: "Dragon Ball" },
-  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
-  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
-  { id: 5, personaje: "Edward Elric", anime: "Fullmetal Alchemist: Brotherhood"},
-  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
+  { id: 1, nombre: "firulais", raza: "dalmata" },
+  { id: 2, nombre: "pancho", raza: "chihuahua" },
+  { id: 3, nombre: "pancha", raza: "pit bull" },
+  { id: 4, nombre: "solovino", raza: "gato" },
 ];
 
 class App extends React.Component {
